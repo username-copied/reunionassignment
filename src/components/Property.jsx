25773861b1d2efad8419001af770d272c1ccba02
shreyas <shreyas.waykar123@gmail.com>
2022-11-19 13:04:19 +0530

    Solved href problem

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -10,7 +10,7 @@ const Property = ({ data }) => {
           return (
             <div key={key}>
               <div className="w-full max-w-sm bg-[#123795] rounded-lg shadow-md  dark:border-gray-700">
-                <a href="#a">
+                <a href="/">
                   <img
                     className="p-8 rounded-t-lg hover:scale-95 duration-200 rounded-lg"
                     src={data.imgSrc}
@@ -18,7 +18,7 @@ const Property = ({ data }) => {
                   />
                 </a>
                 <div className="px-5 pb-5">
-                  <a href="#a">
+                  <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                       <span>Property number : </span>
                       {data.id}
@@ -49,7 +49,7 @@ const Property = ({ data }) => {
                       {data.price}
                     </span>
                     <a
-                      href="#a"
+                      href="/"
                       className="text-white bg-[#4276fa] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                       Take a look
